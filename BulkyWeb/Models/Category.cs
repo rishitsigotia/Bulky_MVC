cfae8340687ac403a99aaaa0b726e2249f1651ca
Rishit <rsigotia123@gmail.com>
2025-06-08 01:04:35 +0530

    Data Annotations for key and requried

--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -1,8 +1,12 @@
-﻿namespace BulkyWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
 {
 	public class Category
 	{
-        public int Id { get; set; }
+		[Key] //data annotation for primary key
+        public int Id { get; set; } //Primary Key 
+		[Required] 
 		public string Name { get; set; }
 		public int DisplayOrder { get; set; }
     }
